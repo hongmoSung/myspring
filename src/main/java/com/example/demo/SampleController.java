1376cc5abdd8824e94693146dda3187f5e19db4a
hongmoSung <hm.sung@softwareinlife.com>
2021-01-11 14:44:36 +0900

    initial commit

---
+++ src/main/java/com/example/demo/SampleController.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SampleController {
+
+    @GetMapping("/sample")
+    public @ResponseBody String sample() {
+        return "sample";
+    }
+
+    @GetMapping("/sample2")
+    public @ResponseBody String sample2() {
+        return "sample2";
+    }
+}
