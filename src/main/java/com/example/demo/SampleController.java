9fd33cb1f799ea66b8dbc2a2f52c551d26fb943c
hongmoSung <hm.sung@softwareinlife.com>
2021-01-12 11:02:57 +0900

    webhook test

--- src/main/java/com/example/demo/SampleController.java
+++ src/main/java/com/example/demo/SampleController.java
@@ -9,10 +9,10 @@ public class SampleController {
 
     @GetMapping("/sample")
     public @ResponseBody String sample() {
-        return "수정 테스트";
+        return "webhook test";
     }
 
-    @GetMapping("/sample2")
+        @GetMapping("/sample2")
     public @ResponseBody String sample2() {
         return "sample2";
     }
